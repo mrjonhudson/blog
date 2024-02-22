7c689cc251750e448da7b1003a40fc13988c5253
mrjonhudson <mrjonhudson@gmail.com>
2024-02-22 19:33:23 +0000

    Added GitHub

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaEnvelope, FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+import { FaEnvelope, FaGithub, FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 
 const Header = () => {
     return (
@@ -9,16 +9,19 @@ const Header = () => {
                 <div className="flex flex-col md:flex-row w-[768px] gap-3 justify-center md:justify-between">
                     <h1 className="text-3xl font-semibold justify-center flex p-3">blog.mrjonhudson</h1>
                     <div className="flex flex-row gap-5 justify-center">
-                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={""}>
+                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={"https://www.linkedin.com/in/mrjonhudson"}>
                             <FaLinkedin />
                         </Link>
-                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={""}>
+                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={"https://www.twitter.com/mrjonhudson"}>
                             <FaXTwitter />
                         </Link>
-                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={""}>
+                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={"https://www.instagram.com/mrjonhudson"}>
                             <FaInstagram />
                         </Link>
-                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={""}>
+                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={"https://github.com/mrjonhudson"}>
+                            <FaGithub />
+                        </Link>
+                        <Link className="flex items-center justify-center text-xl md:text-3xl text-gray-500 hover:text-[var(--dark-void)]" href={"mailto:mrjonhudson@gmail.com"}>
                             <FaEnvelope />
                         </Link>
                     </div>
