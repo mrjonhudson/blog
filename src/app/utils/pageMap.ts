14c366dc51c953afc54b26067bfa53c2b6e6980a
mrjonhudson <mrjonhudson@gmail.com>
2024-03-10 17:30:01 +0000

    Added pages

--- src/app/utils/pageMap.ts
+++ src/app/utils/pageMap.ts
@@ -7,4 +7,6 @@ export const map = {
     spad: '3c668b4b-d71e-4b02-a56a-9263c082e05b',
     startups: '9fd8e874-31d5-4d1c-b60f-63c58d6cb0df',
     tidal_wave: "2ca9c6cf-b25e-487f-abd4-63015d9c696d",
+    consumer: 'a141e912-c034-4212-860c-3984c2d9ea15',
+    app_template: 'ec51ffeb-4e89-488f-b9f1-9e7ff271d4b1'
 }
